002e368d9c4984464664e890cce517cb6b9ae446
Sabbir Rahman <121510193+iconsabbir@users.noreply.github.com>
2023-07-23 17:13:02 +0600

    Add files via upload

---
+++ Blance.js
@@ -0,0 +1,84 @@
+
+// input fild
+
+
+function getInput(inputID){
+    const inputField = document.getElementById(inputID)
+    const inputValue = inputField.value;
+    const inputAmount = parseFloat(inputValue);
+    
+    return inputAmount;
+   
+}
+
+function pastValue(pastAmount){
+    const pastFild = document.getElementById(pastAmount)
+    const pastValue = pastFild.innerText;
+    const pastNumbers = parseFloat(pastValue);
+    
+    return pastNumbers;
+    
+}
+
+
+function updateMoney(updateId, inputAmount,pastAmount, blance,isAdded){
+    const newFild = document.getElementById(updateId)
+    const newAmount = inputAmount + pastAmount;
+    newFild.innerText = newAmount
+    if(isAdded == true){
+    const newBlance = inputAmount + blance;
+    document.getElementById('Blance-amo').innerText = newBlance
+
+    }else{
+        const newBlance = blance - inputAmount;
+        document.getElementById('Blance-amo').innerText = newBlance
+    }
+
+
+    
+    
+}
+
+
+document.getElementById('deposit-button').addEventListener('click', function(){
+    
+    const inputDepositAmount = getInput('deposit-fild');
+    if(isNaN(inputDepositAmount)){
+        alert ('Enter Your Vailed Number')
+    }else{
+        const pastDepositMoney = pastValue('deposit-amo')
+        const presentmoney = pastValue('Blance-amo')
+        updateMoney('deposit-amo', inputDepositAmount,pastDepositMoney, presentmoney, true)
+    }
+    
+
+})
+
+document.getElementById('whitdraw-button').addEventListener('click', function(){
+    getInput('whitdraw-fild');
+    const inputwithdrawAmount = getInput('whitdraw-fild')
+    if(isNaN(inputwithdrawAmount)){
+        alert ('Enter Your Vailed Number')
+    }else{
+        const pastwithdrawMoney = pastValue('whitdraw-amo')
+        const presentmoney = pastValue('Blance-amo')
+        if(inputwithdrawAmount > presentmoney){
+            alert('You Dont Have sufficient blance')
+        }else{
+            updateMoney('whitdraw-amo', inputwithdrawAmount,pastwithdrawMoney, presentmoney, false)
+        }
+
+       
+    }
+    
+
+})
+
+
+
+
+
+
+
+
+
